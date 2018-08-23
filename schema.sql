0b026245a75508a4cd60dd4c23a49a05563cb7a5
Brandon Humphries <brandon.j.humphries@gmail.com>
2018-08-23 14:54:02 -0400

    updated to pull from psql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contacts (
+    databaseid serial PRIMARY KEY,
+    name character varying(255),
+    email character varying(255),
+    phone character varying(255),
+    address character varying(255),
+    id character varying(255)
+);
